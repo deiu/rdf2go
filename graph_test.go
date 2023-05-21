2e0b2c1fd865cf5e3dac6fdf09c1e2fca2ee11d0
davidaf3 <david.alvarez.fidalgo@gmail.com>
2023-05-21 17:49:57 +0200

    Fixed tests

--- graph_test.go
+++ graph_test.go
@@ -78,7 +78,7 @@ func TestGraphLiteralTerms(t *testing.T) {
 }
 
 func TestGraphBlankNodeTerms(t *testing.T) {
-	t1 := NewBlankNode(1)
+	t1 := NewBlankNode("n1")
 	assert.True(t, t1.Equal(rdf2term(term2rdf(t1))))
 	assert.True(t, t1.Equal(jterm2term(term2jterm(t1))))
 }
@@ -199,7 +199,7 @@ func TestSerializeJSONLD(t *testing.T) {
 	g := NewGraph(testUri)
 	g.Parse(strings.NewReader(simpleTurtle), "text/turtle")
 	g.Add(NewTriple(NewResource(testUri+"#me"), NewResource("http://xmlns.com/foaf/0.1/nick"), NewLiteralWithLanguage("test", "en")))
-	g.Add(NewTriple(NewBlankNode(9), NewResource("http://xmlns.com/foaf/0.1/name"), NewLiteralWithLanguage("test", "en")))
+	g.Add(NewTriple(NewBlankNode("n9"), NewResource("http://xmlns.com/foaf/0.1/name"), NewLiteralWithLanguage("test", "en")))
 	assert.Equal(t, 4, g.Len())
 
 	var b bytes.Buffer
